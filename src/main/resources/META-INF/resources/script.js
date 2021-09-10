8d8eb5f4e8c1ae1eb9228c8aa37f86013598b800
SeiyaB04 <51695602+SeiyaB04@users.noreply.github.com>
2021-09-10 15:49:51 +0200

    Programm all login functions for frontend

--- src/main/resources/META-INF/resources/script.js
+++ src/main/resources/META-INF/resources/script.js
@@ -1,129 +1,67 @@
 const URL = 'http://localhost:8080';
-let entries = [];
 let users = [];
 
-const dateAndTimeToDate = (dateString, timeString) => {
-    return new Date(`${dateString}T${timeString}`).toISOString();
-};
 
-const createEntry = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const entry = {};
-    entry['checkIn'] = dateAndTimeToDate(formData.get('checkInDate'), formData.get('checkInTime'));
-    entry['checkOut'] = dateAndTimeToDate(formData.get('checkOutDate'), formData.get('checkOutTime'));
-
-    fetch(`${URL}/entries`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(entry)
-    }).then((result) => {
-        result.json().then((entry) => {
-            entries.push(entry);
-            renderEntries();
-        });
-    });
-};
-
-const indexEntries = () => {
-    fetch(`${URL}/entries`, {
-        method: 'GET'
-    }).then((result) => {
-        result.json().then((result) => {
-            entries = result;
-            renderEntries();
-        });
-    });
-    renderEntries();
-};
-
-const createCell = (text) => {
-    const cell = document.createElement('td');
-    cell.innerText = text;
-    return cell;
-};
-
-const renderEntries = () => {
-    const display = document.querySelector('#entryDisplay');
-    display.innerHTML = '';
-    entries.forEach((entry) => {
-        const row = document.createElement('tr');
-        row.appendChild(createCell(entry.id));
-        row.appendChild(createCell(new Date(entry.checkIn).toLocaleString()));
-        row.appendChild(createCell(new Date(entry.checkOut).toLocaleString()));
-        display.appendChild(row);
-    });
-};
-
-document.addEventListener('DOMContentLoaded', function(){
-    const createEntryForm = document.querySelector('#createEntryForm');
-    createEntryForm.addEventListener('submit', createEntry);
-    indexEntries();
-});
+//Login----------------------------------------------------------------------------------------------
 
 const register = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
     const register = {};
-    register['username'] = (formData.get('registerUsername'));
-    register['password'] = (formData.get('registerPassword'));
+    const formData = new FormData(document.getElementById('registerForm'));
 
-    fetch(`${URL}/users`, {
+    register['username'] = formData.get('registerUsername');
+    register['password'] = formData.get('registerPassword');
+
+    fetch(`${URL}/auth/signUp`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
         },
         body: JSON.stringify(register)
-    }).then((result) => {
-        result.json().then((register) => {
-            users.push(register);
-            renderUser();
-        });
     });
 };
 
-const renderUser = () => {
-    const display = document.querySelector('#userDisplay');
-    display.innerHTML = '';
-
-    users.forEach((user) => {
-        const row = document.createElement('tr');
-        row.appendChild(createCell(user.id));
-        row.appendChild(createCell(user.username));
-        row.appendChild(createCell(user.password));
-        display.appendChild(row);
-    })
-};
-
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const registerForm = document.querySelector('#registerForm');
     registerForm.addEventListener('submit', register);
-    indexEntries();
 });
 
-/*const login = (e) => {
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.querySelector('#loginForm');
+    loginForm.addEventListener('submit', login);
+});
+
+
+async function login(e) {
     e.preventDefault();
-    const formData = new FormData(e.target);
+    const formData = new FormData(document.getElementById('loginForm'));
+
     const login = {};
     login['username'] = (formData.get('loginUsername'));
     login['password'] = (formData.get('loginPassword'));
 
-    if (login['username'] == )
-
-    fetch(`${URL}/users`, {
-        method: 'GET',
+    const response = await fetch(`${URL}/auth/login`, {
+        method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
         },
-        body: JSON.stringify(register)
-    }).then((result) => {
-        result.json().then((register) => {
-            users.push(register);
-            renderUser();
-        });
+        body: JSON.stringify(login)
     });
-};
 
- */
+    let jwt;
+    jwt = await response.json();
+
+    if (jwt != null) {
+        localStorage.setItem('token',jwt.token);
+        location.replace('http://localhost:8080/entries.html');
+    }
+}
+
+function logout() {
+    localStorage.clear();
+    location.replace('http://localhost:8080/index.html');
+}
+
+
